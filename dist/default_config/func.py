08349ec115fa77120143f70dae9b1e7a1362d45e
JulioLeGrandSage <112372790+juliolegrandsage@users.noreply.github.com>
2026-09-20 20:07:31 +0200

    Add files via upload

---
+++ dist/default_config/func.py
@@ -0,0 +1,11 @@
+import subprocess
+import webbrowser
+# import os
+
+
+# You can add your own commands in this file by creating a function and giving it a midi note in "config.json"
+
+
+def hello_mideck():
+    print("Connected")
+
